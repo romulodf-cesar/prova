38fc9b6379a2523a50404a0ef5529da04372af50
romulodf-cesar <romulodf.cesar@gmail.com>
2026-09-02 20:58:48 -0300

    ok

--- motorartigos/urls.py
+++ motorartigos/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from motorartigos.views import index,artigo,detalhe_comercio,mostrar_dashboard
+from motorartigos.views import index, artigo, detalhe_comercio, mostrar_dashboard, update_server
 
 urlpatterns = [
-    path('',index,name='index'),
-    path('artigo/',artigo,name='artigo'),
+    path('', index, name='index'),
+    path('artigo/', artigo, name='artigo'),
     path('artigo/<int:id>/', detalhe_comercio, name='detalhe_comercio'),
     path('dashboard/', mostrar_dashboard, name='mostrar_dashboard'),
- ]
+    path('update_server/', update_server, name='update_server'),
+]
